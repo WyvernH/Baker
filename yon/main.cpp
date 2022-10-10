e8681bb1ed8f202f7cef15a493bd893e9f5fea30
Matthew Hinton <93463398+WyvernH@users.noreply.github.com>
2022-10-10 08:27:26 -0700

    Add checking for well-formed input

--- yon/main.cpp
+++ yon/main.cpp
@@ -14,9 +14,9 @@ int main(void) {
 	int decPrecision{ }, yonPrecision{ }, radixCount{ };
 	std::string decimalCharacters{".0123456789"};
 	std::string yonnum{""}, decnum{""};
-	bool decimal;
+	bool decimal, ignoreZeroFraction;
 
-	std::cout << "\nType \"q\" to quit\n\n";
+	std::cout << "\nType \"q\" to quit...\n\n";
 
 	while (true) {
 		yonnum = "";
@@ -48,42 +48,49 @@ int main(void) {
 						decnum.erase(decnum.begin());
 					}
 				}
-				while (decnum.size() > 0 && decnum.find(".") != 1) {
-					if (decnum.back() != decimalCharacters.at(1)) {
-						break;
-					} else {
-						decnum.pop_back();
-						++decPrecision;
+				if (radixCount == 1) {
+					while (decnum.size() > 0 && decnum.back() != '.') {
+						if (decnum.back() != decimalCharacters.at(1)) {
+							break;
+						} else {
+							decnum.pop_back();
+							++decPrecision;
+						}
 					}
 				}
 				if (decnum == "") {
 					yonnum == "0";
 				} else {
+					if (IGNORE_ZERO_FRACTION && decnum.back() == '.') {
+						ignoreZeroFraction = true;
+					} else { ignoreZeroFraction = false; }
 					if (decnum.find(".") != std::string::npos) {
 						decPrecision += decnum.size() - decnum.find(".");
 					}
-					if (decnum.find(".") != std::string::npos && decnum.find(".") > MAX_PRECISION) {
+					if (decnum.find(".") != std::string::npos && (decnum.find(".") > MAX_PRECISION || decnum.size() - decnum.find(".") - 1 > MAX_PRECISION) ) {
+						yonnum = "E3";
+					} else if (decnum.find(".") == std::string::npos && decnum.size() > MAX_PRECISION) {
+						yonnum = "E3";
+					} else if (TRUNC_PRECISION == false && decPrecision > MAX_PRECISION) {
 						yonnum = "E3";
 					} else {
-						if (TRUNC_PRECISION == false && decPrecision > MAX_PRECISION) {
-							yonnum = "E3";
-						} else {
-							if (decPrecision > MAX_PRECISION) {
-								decPrecision = MAX_PRECISION + 1;
-							}
-							--decPrecision;
-							dec = stold(decnum);
-
-							yonPrecision = 0;
-							if (EXTRA_PRECISION) {
-								if (decPrecision > 0) {
-									yonPrecision = decPrecision;
-								}
-							} else {
-							yonPrecision = floor( log( pow(10, decPrecision) ) / log(13) );
-							}
-							yonnum = yon(dec, yonPrecision);
+						if (decPrecision > MAX_PRECISION) {
+							decPrecision = MAX_PRECISION + 1;
 						}
+						--decPrecision;
+						if (decnum.find(".") == 0) { decnum = "0" + decnum; }
+						if (decnum.find(".") == decnum.size() - 1) { decnum = decnum + "0"; }
+						dec = stold(decnum);
+
+						yonPrecision = 0;
+						if (EXTRA_PRECISION) {
+							if (decPrecision > 0) {
+								yonPrecision = decPrecision;
+							}
+						} else {
+						yonPrecision = floor( log( pow(10, decPrecision) ) / log(13) );
+						}
+						yonnum = yon(dec, yonPrecision, ignoreZeroFraction);
 					}
 				}
 			} else {
@@ -92,13 +99,13 @@ int main(void) {
 		}
 
 		if (yonnum == "E1") {
-			std::cout << "\nThis blasphemous number is not holy enough to be displayed in Baker's Dozenal without offending our lord and saviour ...\n\n\n";
+			std::cout << "\nThis blasphemous number is not holy enough to be displayed in Baker's Dozenal without offending our lord and saviour Francois.\n\n\n";
 		} else if (yonnum == "E2") {
-			std::cout << "\nThis number is too holy to be converted by a mere command line program. You should do this in your head to avoid offending our lord and saviour ...\n\n\n";
+			std::cout << "\nThis number is too holy to be converted by a mere command line program. You should do this in your head to avoid offending our lord and saviour Francois.\n\n\n";
 		} else if (yonnum == "E3") {
 			std::cout << "\nA number this long and complicated aught to be kept in the mind, lest we risk a dark forest strike from a malevolent civilization with sophon technology.\n\n\n";
 		} else if (yonnum == "E4") {
-			std::cout << "\nThis number doesn't obey all the known laws of mathematics. If you are reading this, it is most likely a message from our lord and saviour.\n\n\n";
+			std::cout << "\nThis number doesn't obey all the known laws of mathematics. If you are reading this, it is most likely a message from our lord and saviour Francois.\n\n\n";
 		} else if (yonnum == "") {
 			std::cout << "Baker's Dozenal: 0\n\n";
 		} else {
--- zyon/main.cpp
+++ zyon/main.cpp
@@ -15,9 +15,9 @@ int main(void) {
 	int dozPrecision{ }, yonPrecision{ }, radixCount{ };
 	std::string dozenalCharacters{".0123456789AaBbXxEe"};
 	std::string yonnum{""}, doznum{""};
-	bool dozenal;
+	bool dozenal, ignoreZeroFraction;
 
-	std::cout << "\nType \"q\" to quit\n\n";
+	std::cout << "\nType \"q\" to quit...\n\n";
 
 	while (true) {
 		yonnum = "";
@@ -49,42 +49,47 @@ int main(void) {
 						doznum.erase(doznum.begin());
 					}
 				}
-				while (doznum.size() > 0 && doznum.find(".") != 1) {
-					if (doznum.back() != dozenalCharacters.at(1)) {
-						break;
-					} else {
-						doznum.pop_back();
-						++dozPrecision;
+				if (radixCount == 1) {
+					while (doznum.size() > 0 && doznum.back() != '.') {
+						if (doznum.back() != dozenalCharacters.at(1)) {
+							break;
+						} else {
+							doznum.pop_back();
+							++dozPrecision;
+						}
 					}
 				}
 				if (doznum == "") {
 					yonnum = "0";
 				} else {
+					if (IGNORE_ZERO_FRACTION && doznum.back() == '.') {
+						ignoreZeroFraction = true;
+					} else { ignoreZeroFraction = false; }
 					if (doznum.find(".") != std::string::npos) {
 						dozPrecision += doznum.size() - doznum.find(".");
 					}
-					if (doznum.find(".") != std::string::npos && doznum.find(".") > MAX_PRECISION) {
+					if (doznum.find(".") != std::string::npos && (doznum.find(".") > MAX_PRECISION || doznum.size() - doznum.find(".") - 1 > MAX_PRECISION) ) {
+						yonnum = "E3";
+					} else if (doznum.find(".") == std::string::npos && doznum.size() > MAX_PRECISION) {
+						yonnum = "E3";
+					} else if (TRUNC_PRECISION == false && dozPrecision > MAX_PRECISION) {
 						yonnum = "E3";
 					} else {
-						if (TRUNC_PRECISION == false && dozPrecision > MAX_PRECISION) {
-							yonnum = "E3";
-						} else {
-							if (dozPrecision > MAX_PRECISION) {
-								dozPrecision = MAX_PRECISION + 1;
-							}
-							--dozPrecision;
-							dec = doztodec(doznum);
-
-							yonPrecision = 0;
-							if (EXTRA_PRECISION) {
-								if (dozPrecision > 0) {
-									yonPrecision = dozPrecision;
-								}
-							} else {
-								yonPrecision = floor( log( pow(12, dozPrecision) ) / log(13) );
-							}
-							yonnum = yon(dec, yonPrecision);
+						if (dozPrecision > MAX_PRECISION) {
+							dozPrecision = MAX_PRECISION + 1;
 						}
+						--dozPrecision;
+						dec = doztodec(doznum);
+
+						yonPrecision = 0;
+						if (EXTRA_PRECISION) {
+							if (dozPrecision > 0) {
+								yonPrecision = dozPrecision;
+							}
+						} else {
+							yonPrecision = floor( log( pow(12, dozPrecision) ) / log(13) );
+						}
+						yonnum = yon(dec, yonPrecision, ignoreZeroFraction);
 					}
 				}
 			} else {
@@ -93,13 +98,13 @@ int main(void) {
 		}
 
 		if (yonnum == "E1") {
-			std::cout << "\nThis blasphemous number is not holy enough to be displayed in Baker's Dozenal without offending our lord and saviour ...\n\n\n";
+			std::cout << "\nThis blasphemous number is not holy enough to be displayed in Baker's Dozenal without offending our lord and saviour Francois.\n\n\n";
 		} else if (yonnum == "E2") {
-			std::cout << "\nThis number is too holy to be converted by a mere command line program. You should do this in your head to avoid offending our lord and saviour ...\n\n\n";
+			std::cout << "\nThis number is too holy to be converted by a mere command line program. You should do this in your head to avoid offending our lord and saviour Francois.\n\n\n";
 		} else if (yonnum == "E3") {
 			std::cout << "\nA number this long and complicated aught to be kept in the mind, lest we risk a dark forest strike from a malevolent civilization with sophon technology.\n\n\n";
 		} else if (yonnum == "E4") {
-			std::cout << "\nThis number doesn't obey all the known laws of mathematics. If you are reading this, it is most likely a message from our lord and saviour.\n\n\n";
+			std::cout << "\nThis number doesn't obey all the known laws of mathematics. If you are reading this, it is most likely a message from our lord and saviour Francois.\n\n\n";
 		} else if (yonnum == "") {
 			std::cout << "Baker's Dozenal: 0\n\n";
 		} else {
